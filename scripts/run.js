4a88b5515f4bdc1e53dd2ba821606661112f798d
Joey <joey@xm8.ninja>
2021-10-23 15:52:31 -0400

    update to MyEpicGame.sol and run.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,15 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Juan Soto", "Bryce Harper", "Cool Papa Bell"], // Names
+    [
+      "https://i.imgur.com/JZTVdZw.jpeg", // Images
+      "https://i.imgur.com/oNbU8Lv.jpeg",
+      "https://i.imgur.com/kNfA7ug.jpeg",
+    ],
+    [100, 200, 300], // HP values
+    [100, 50, 25] // Attack damage values
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
